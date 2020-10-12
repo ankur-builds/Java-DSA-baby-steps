53335128bccfeda92aaf55d2619f2128d18ae01f
theGandiva <ankur8agrr@gmail.com>
2020-10-12 18:49:21 +0530

    My experiments with tree traversals

--- src/practice/tree/traversals/FindPostOrder.java
+++ src/practice/tree/traversals/FindPostOrder.java
@@ -12,6 +12,11 @@
 
 package practice.tree.traversals;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class FindPostOrder{
     static int preIndex = 0;
 
@@ -27,7 +32,7 @@ public class FindPostOrder{
         /*
           Here we are traversing left and right subtree across parent/root just like how we do in
           postorder traversal. Important thing to note here is that we are not constructing a tree
-          but with indx obtained in previous line, we are able to divide our traversal/search across it.
+          but with indx obtained in previous line, we are able to divide our tree traversal across it.
          */
         printPostOrder(inorderArray, preorderArray, start, indx-1);
         printPostOrder(inorderArray, preorderArray, indx+1, end);
@@ -73,6 +78,108 @@ public class FindPostOrder{
         return end+1;
     }
 
+    void findLRIndex(int[] preorderArray){
+        int leftmost = 0, rightmost = 0;
+        int min = Integer.MAX_VALUE;
+        int max = Integer.MIN_VALUE;
+
+        for(int i = 0; i<preorderArray.length; ++i){
+            if(preorderArray[i]<min){
+                min = preorderArray[i];
+                leftmost = i;
+            }
+            if(preorderArray[i]>max){
+                max = preorderArray[i];
+                rightmost = i;
+            }
+        }
+
+        System.out.println("Leftmost : " + preorderArray[leftmost] + " Rightmost : " + preorderArray[rightmost]);
+    }
+
+    // Problem statement 3 : Replace each node with sum of its inorder predecessor and successor
+    public int[] replaceNode(int[] arr){
+        int[] output = new int[arr.length];
+
+        output[0] = arr[1];
+        output[arr.length-1] = arr[arr.length-2];
+        for(int i = 1; i<arr.length - 1; ++i){
+            output[i] = arr[i-1]+arr[i+1];
+        }
+
+        return output;
+    }
+
+    // Problem statement 4 : Populate inorder successor for all nodes
+    List<Node> successorList = new ArrayList<>();
+    void populateSuccessor(Node n){
+        if(n==null)
+            return;
+
+        populateSuccessor(n.left);
+        System.out.print(n.data + " ");
+        successorList.add(n);
+        populateSuccessor(n.right);
+    }
+
+    void printSuccessorList(){
+        for(int i = 1; i<6; ++i){
+            addNode(ThreadLocalRandom.current().nextInt(40));
+        }
+
+        System.out.println("\n");
+        populateSuccessor(root);
+
+        for(int i = 0; i<successorList.size() - 1; ++i){
+            successorList.get(i).next = successorList.get(i+1);
+        }
+
+        successorList.get(successorList.size() - 1).next = null;
+
+        System.out.println("\n");
+        for (Node node : successorList) {
+            if (node.next == null)
+                System.out.println(0);
+            else
+                System.out.print(node.next.data + " ");
+        }
+    }
+    void addNode(int d){
+        System.out.print(d + " ");
+        if(root==null) {
+            root = new Node(d);
+            return;
+        }
+
+        Node n = root;
+        while(n!=null){
+            if(n.data > d) {
+                if(n.left==null) {
+                    n.left = new Node(d);
+                    return;
+                } else
+                    n =n.left;
+            } else {
+                if(n.right==null) {
+                    n.right = new Node(d);
+                    return;
+                } else
+                    n =n.right;
+            }
+        }
+    }
+
+    private Node root;
+    static class Node{
+        int data;
+        Node left, right, next;
+
+        Node(int d){
+            data = d;
+            left = right = next = null;
+        }
+    }
+
     public static void main(String[] ars)
     {
         int[] in = { 4, 2, 5, 1, 3, 6 };
@@ -82,9 +189,17 @@ public class FindPostOrder{
         treeNode.printPostOrder(in, pre, 0, len-1);
 
         System.out.println();
-        int[] preorder = {40, 30, 35, 37, 39, 80, 100};
+        int[] preorder = {40, 30, 26, 35, 37, 39, 80, 100};
         len = preorder.length;
         treeNode.printPostOrderTraversal(preorder, 0, len-1);
         System.out.println();
+
+        System.out.println(Arrays.toString(preorder));
+        treeNode.findLRIndex(preorder);
+
+        int[] inorder = {4, 2, 5, 1, 6, 3, 7};
+        System.out.println(Arrays.toString(treeNode.replaceNode(inorder)));
+
+        treeNode.printSuccessorList();
     }
 }
---
+++ src/practice/tree/traversals/LevelOrder.java
@@ -0,0 +1,19 @@
+/*
+ * Copyright (c) 2020.
+ * File : LevelOrder.java
+ * Author : Ankur
+ * Last modified : 6/10/2020
+ * Problem Statement at the end of the code
+ *
+ * All code is for practice purpose only and strictly non-commercial.
+ * All rights reserved.
+ * Please refer to apache license terms in the project.
+ */
+
+package practice.tree.traversals;
+
+public class LevelOrder {
+    public static void main(String[] args) {
+
+    }
+}
