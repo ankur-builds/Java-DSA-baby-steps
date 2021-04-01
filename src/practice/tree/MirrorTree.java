f1f1fb1cf31f80d8d254f891033b44d73d212deb
theGandiva <ankur8agrr@gmail.com>
2021-04-01 23:37:51 +0530

    Mirror a BST

--- src/practice/tree/MirrorTree.java
+++ src/practice/tree/MirrorTree.java
@@ -13,9 +13,15 @@
 package practice.tree;
 
 import dsa.BST;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class MirrorTree {
+    // When we take mirror of a binary, left & right child of every node will get swapped.
+    // Its implementation in recursive approach.
     public static void mirror(BST.Node n){
         if(n==null)
             return;
@@ -26,6 +32,36 @@ public class MirrorTree {
         BST.Node temp = n.left;
         n.left = n.right;
         n.right = temp;
+    }
+
+    // Here I am not reconstructing the tree but just mirroring the output using iterative approach
+    public static void mirrorIteratively(BST.Node n){
+        Queue<BST.Node> q = new LinkedList<>();
+        Stack<Integer> stk = new Stack<>();
+
+        q.add(n);
+        q.add(null);
+        while(!q.isEmpty()){
+            n = q.poll();
+
+            if(n!=null){
+                stk.add(n.data);
+                if(n.left!=null)
+                    q.add(n.left);
+                if(n.right!=null)
+                    q.add(n.right);
+            } else{
+                // Basically here I am just reversing the level order traversal using stack
+                while(!stk.isEmpty())
+                    System.out.print(stk.pop() + " ");
+
+                System.out.println();
+                if(q.isEmpty())
+                    return;
+                else
+                    q.add(null);
+            }
+        }
     }
 
     public static void main(String[] args) {
@@ -41,5 +77,8 @@ public class MirrorTree {
         System.out.println("------");
         mirror(t.root);
         t.printLevelOrderTraversal(t.root);
+
+        System.out.println("------");
+        mirrorIteratively(t.root);
     }
 }
