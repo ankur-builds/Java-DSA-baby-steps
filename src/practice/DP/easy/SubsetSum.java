cebdf99d2a4c9dd659d9cb9062f5d87c471e7648
theGandiva <ankur8agrr@gmail.com>
2021-07-02 20:31:09 +0530

    DP : Count number of subsets of a given sum

--- src/practice/DP/easy/SubsetSum.java
+++ src/practice/DP/easy/SubsetSum.java
@@ -12,6 +12,8 @@
 
 package practice.DP.easy;
 
+import java.util.Arrays;
+
 public class SubsetSum {
     static boolean recursion(int[] arr, int indx, int total){
         if(indx==arr.length)
@@ -46,6 +48,7 @@ public class SubsetSum {
         return table[arr.length][total];
     }
 
+    // Variation 1 : Find if its possible to partitions an array into two segments such that their sum is equal.
     static boolean equalSumPartition(int[] arr){
         int sum = 0;
         for (int j : arr) {
@@ -58,11 +61,35 @@ public class SubsetSum {
         return tabulation(arr, sum/2);
     }
 
+    // Variation 2 : Count number of subsets of a given sum
+    static int countSubsets(int[] arr, int sum){
+        int[][] table = new int[arr.length+1][sum+1];
+
+        for(int i = 0; i<=arr.length; ++i){
+            table[0][i] = 0;
+            table[i][0] = 1;
+        }
+
+        for(int i = 1; i<=arr.length; ++i){
+            for(int j =1; j<=sum; ++j){
+                if(arr[i-1]>j)
+                    table[i][j] = table[i-1][j];
+                else
+                    table[i][j] = table[i-1][j]+table[i-1][j-arr[i-1]];
+            }
+        }
+
+        return table[arr.length][sum];
+    }
+
     public static void main(String[] args) {
         int[] set = {3, 34, 4, 12, 5, 2};
         System.out.println(recursion(set,0,60));
         System.out.println(tabulation(set,60));
 
         System.out.println("Equal Sum Partition possible : " + equalSumPartition(set));
+
+        int[] arr = {2,3,5,6,8,10};
+        System.out.println(countSubsets(arr, 10));
     }
 }
