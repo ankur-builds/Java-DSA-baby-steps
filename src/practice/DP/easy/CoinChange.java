92b30a98771f7af721944f7f2a333e8917e65126
theGandiva <ankur8agrr@gmail.com>
2021-07-04 18:21:19 +0530

    DP : Min number of ways to get sum(Coin Change Problem)

--- src/practice/DP/easy/CoinChange.java
+++ src/practice/DP/easy/CoinChange.java
@@ -36,8 +36,29 @@ public class CoinChange {
         return table[coin.length][sum];
     }
 
+    static int minNumberOfWays(int[] coin, int sum){
+        int[][] table = new int[coin.length+1][sum+1];
+
+        for(int j = 1; j<=sum; ++j){
+            table[0][j] = Integer.MAX_VALUE;
+        }
+
+        for(int i = 1; i<= coin.length; ++i){
+            for(int j = 1; j<=sum; ++j){
+                if(coin[i-1]>j)
+                    table[i][j] = table[i-1][j];
+                else
+                    table[i][j] = Math.min(table[i-1][j], table[i][j-coin[i-1]]+1);
+            }
+        }
+
+        return table[coin.length][sum];
+    }
+
     public static void main(String[] args) {
         int[] arr = {1,2,3};
         System.out.println(tabulation(arr, 5));
+
+        System.out.println(minNumberOfWays(arr,5));
     }
 }
