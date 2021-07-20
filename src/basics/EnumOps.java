7621c7066b96fea9b067300c7b32b123177882a0
theGandiva <ankur8agrr@gmail.com>
2021-07-20 12:59:53 +0530

    Cleanup

--- src/basics/EnumOps.java
+++ src/basics/EnumOps.java
@@ -12,7 +12,7 @@
 
 package basics;
 
-public enum Planet {
+public enum EnumOps {
     MERCURY (3.303e+23, 2.4397e6),
     VENUS   (4.869e+24, 6.0518e6),
     EARTH   (5.976e+24, 6.37814e6),
@@ -24,7 +24,7 @@ public enum Planet {
 
     private final double mass;   // in kilograms
     private final double radius; // in meters
-    Planet(double mass, double radius) {
+    EnumOps(double mass, double radius) {
         this.mass = mass;
         this.radius = radius;
     }
@@ -40,14 +40,15 @@ public enum Planet {
     double surfaceWeight(double otherMass) {
         return otherMass * surfaceGravity();
     }
+
     public static void main(String[] args) {
         if (args.length != 1) {
-            System.err.println("Usage: java Planet <earth_weight>");
+            System.err.println("Pls provide <earth_weight> to check your weight on other planets");
             System.exit(-1);
         }
         double earthWeight = Double.parseDouble(args[0]);
         double mass = earthWeight/EARTH.surfaceGravity();
-        for (Planet p : Planet.values())
+        for (EnumOps p : EnumOps.values())
             System.out.printf("Your weight on %s is %f%n",
                     p, p.surfaceWeight(mass));
     }
