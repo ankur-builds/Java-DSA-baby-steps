91cb502529ec5353b7806357a44ca77ed9b70fac
theGandiva <ankur8agrr@gmail.com>
2021-08-19 22:26:33 +0530

    Rearrange an array such that arr[i] = i

--- src/practice/array/rearrangement/ArrayRearrangement.java
+++ src/practice/array/rearrangement/ArrayRearrangement.java
@@ -12,6 +12,30 @@
 
 package practice.array.rearrangement;
 
-public class ArrayRearrangement {
+import java.util.Arrays;
 
+// https://www.geeksforgeeks.org/rearrange-array-arri/
+public class ArrayRearrangement {
+    public static void rearrange(int[] arr){
+        int[] output = new int[arr.length];
+        for(int i = 0; i<arr.length; ++i){
+            output[i] = -1;
+        }
+
+        for(int x : arr){
+            if(x>=0 && x<arr.length)
+                output[x] = x;
+        }
+
+        System.out.println(Arrays.toString(arr));
+        System.out.println(Arrays.toString(output));
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {-1, -2, 6, 1, 9, 3, 2, -1, 4, -1};
+        rearrange(arr);
+
+        int[] arr1 = {19, 7, 0, 3, 18, 15, 12, 6, 1, 8, 11, 10, 9, 5, 13, 16, 2, 14, 17, 4};
+        rearrange(arr1);
+    }
 }
