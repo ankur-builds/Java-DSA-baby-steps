fbc6eadf5bcbe396b7b00d820ea4bbd572994f85
theGandiva <ankur8agrr@gmail.com>
2021-09-23 14:54:13 +0530

    Find smallest element in sorted and rotated array

--- src/practice/array/rotations/SearchInRotatedArray.java
+++ src/practice/array/rotations/SearchInRotatedArray.java
@@ -12,37 +12,61 @@
 package practice.array.rotations;
 
 public class SearchInRotatedArray {
-    static int findSmallestIndex(int[] arr){
+    static int findLargestIndex(int[] arr){
         int left = 0, right = arr.length-1, mid = 0;
 
         while(left<=right){
             mid = (left+right)/2;
 
             if(mid==0 || mid==arr.length-1)
-                break;
+                return mid;
 
             if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
-                break;
+                return mid;
             else if(arr[mid]>=arr[mid-1] && arr[mid]>=arr[left])
                 left = mid+1;
             else
                 right = mid-1;
         }
 
-        return (mid==arr.length-1) ? 0 : mid+1;
+        return mid;
+    }
+
+    static int findSmallestIndex(int[] arr){
+        int left = 0, right = arr.length-1, mid = 0;
+
+        while(left<=right){
+            mid = (left+right)/2;
+            if(mid==0 || mid==arr.length-1)
+                return mid;
+
+            // Identify key
+            if(arr[mid]<arr[mid-1] && arr[mid]<arr[mid+1]){
+                return mid;
+            } else if (arr[left]<arr[right] || arr[mid]<=arr[left]){ // Identify where to iterate next
+                right = mid-1;
+            } else
+                left = mid+1;
+        }
+
+        return mid;
     }
 
     public static void main(String[] args) {
         int[] arr = {3,4,5,6,7,8,9,1,2};
-        System.out.println(findSmallestIndex(arr));
+        System.out.print(findSmallestIndex(arr) + " : ");
+        System.out.println(findLargestIndex(arr));
 
         int[] arr1 = {1,2,3,4,5,6,7,8,9};
-        System.out.println(findSmallestIndex(arr1));
+        System.out.print(findSmallestIndex(arr1) + " : ");
+        System.out.println(findLargestIndex(arr1));
 
         int[] arr2 = {5,6,7,8,9,1,2,3,4};
-        System.out.println(findSmallestIndex(arr2));
+        System.out.print(findSmallestIndex(arr2) + " : ");
+        System.out.println(findLargestIndex(arr2));
 
         int[] arr3 = {9,1,2,3,4,5,6,7,8};
-        System.out.println(findSmallestIndex(arr3));
+        System.out.print(findSmallestIndex(arr3) + " : ");
+        System.out.println(findLargestIndex(arr3));
     }
 }
